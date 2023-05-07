23f3b424477baa711ada217fc4a98db8107f8dce
Carlos Estrada <cestr008@gmail.com>
2023-05-07 11:05:15 -0400

    add handleNumber function

--- main.js
+++ main.js
@@ -15,62 +15,75 @@ let operator = null;
 
 const handleOnClick = (value) => {
     if (isNaN(value)) {
-        switch (value) {
-            case 'C':
-                screen = [];
-                leftOperand = null;
-                rightOperand = null;
-                break;
+        handleSymbol(value);
+        // switch (value) {
+        //     case 'C':
+        //         screen = [];
+        //         leftOperand = null;
+        //         rightOperand = null;
+        //         break;
 
-            case '←':
-                screen.pop();
-                break;
+        //     case '←':
+        //         screen.pop();
+        //         break;
 
-            case '×':
-                if (operator === null) {
-                    leftOperand = Number.parseInt(screen.join(''));
-                }
+        //     case '×':
+        //         if (operator === null) {
+        //             leftOperand = Number.parseInt(screen.join(''));
+        //         }
 
-                operator = '×';
-                screen = [];
-                break;
+        //         operator = '×';
+        //         screen = [];
+        //         break;
 
-            case '-':
-                if (operator === null) {
-                    leftOperand = Number.parseInt(screen.join(''));
-                }
+        //     case '-':
+        //         if (operator === null) {
+        //             leftOperand = Number.parseInt(screen.join(''));
+        //         }
 
-                operator = '-';
-                screen = [];
-                break;
+        //         operator = '-';
+        //         screen = [];
+        //         break;
 
-            case '-':
-                if (operator === null) {
-                    leftOperand = Number.parseInt(screen.join(''));
-                }
+        //     case '-':
+        //         if (operator === null) {
+        //             leftOperand = Number.parseInt(screen.join(''));
+        //         }
 
-                operator = '-';
-                screen = [];
-                break;
+        //         operator = '-';
+        //         screen = [];
+        //         break;
 
-            case '=':
-                if (leftOperand === null) break;
+        //     case '=':
+        //         if (leftOperand === null) break;
 
-                rightOperand = Number.parseInt(screen.join(''));
-                calculate();
-                break;
+        //         rightOperand = Number.parseInt(screen.join(''));
+        //         calculate();
+        //         break;
         
-            default:
-                break;
-        }
+        //     default:
+        //         break;
+        // }
     } else {
-        // Do NOT append 0 to 0
-        if (value === '0' && screen.length === 0) return;
+        handleNumber(value);
+        // // Do NOT append 0 to 0
+        // if (value === '0' && screen.length === 0) return;
 
-        screen.push(value)
+        // screen.push(value)
     }
 
     updateScreen();
+}
+
+const handleNumber = (number) => {
+    if (number === '0' && screen.length === 0)
+        return;
+
+    screen.push(number);
+}
+
+const handleSymbol = (symbol) => {
+    console.log('symbol', symbol);
 }
 
 const updateScreen = () => {
